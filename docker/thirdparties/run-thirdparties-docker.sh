ee4805a797ac96723752b11c8718b1b88a46c4b1
zenghua <441651826@qq.com>
2024-04-08 10:08:59 +0800

    add lakesoul regression test
    Signed-off-by: zenghua <441651826@qq.com>

--- docker/thirdparties/run-thirdparties-docker.sh
+++ docker/thirdparties/run-thirdparties-docker.sh
@@ -37,10 +37,13 @@ Usage: $0 <options>
      --stop             stop the specified components
 
   All valid components:
-    mysql,pg,oracle,sqlserver,clickhouse,es,hive,iceberg,hudi,trino,kafka,mariadb,db2
+    mysql,pg,oracle,sqlserver,clickhouse,es,hive,iceberg,hudi,trino,kafka,mariadb,db2,lakesoul
   "
     exit 1
 }
+COMPONENTS=$2
+HELP=0
+STOP=0
 
 if ! OPTS="$(getopt \
     -n "$0" \
@@ -54,13 +57,11 @@ fi
 
 eval set -- "${OPTS}"
 
-COMPONENTS=""
-HELP=0
-STOP=0
+
 
 if [[ "$#" == 1 ]]; then
     # default
-    COMPONENTS="mysql,es,hive,pg,oracle,sqlserver,clickhouse,mariadb,iceberg,db2"
+    COMPONENTS="mysql,es,hive,pg,oracle,sqlserver,clickhouse,mariadb,iceberg,db2,lakesoul"
 else
     while true; do
         case "$1" in
@@ -92,7 +93,7 @@ else
     done
     if [[ "${COMPONENTS}"x == ""x ]]; then
         if [[ "${STOP}" -eq 1 ]]; then
-            COMPONENTS="mysql,es,pg,oracle,sqlserver,clickhouse,hive,iceberg,hudi,trino,kafka,mariadb,db2"
+            COMPONENTS="mysql,es,pg,oracle,sqlserver,clickhouse,hive,iceberg,hudi,trino,kafka,mariadb,db2,lakesoul"
         fi
     fi
 fi
@@ -103,6 +104,7 @@ fi
 
 if [[ "${COMPONENTS}"x == ""x ]]; then
     echo "Invalid arguments"
+    echo ${COMPONENTS}
     usage
 fi
 
@@ -134,6 +136,7 @@ RUN_KAFKA=0
 RUN_SPARK=0
 RUN_MARIADB=0
 RUN_DB2=0
+RUN_LAKESOUL=0
 
 for element in "${COMPONENTS_ARR[@]}"; do
     if [[ "${element}"x == "mysql"x ]]; then
@@ -164,6 +167,8 @@ for element in "${COMPONENTS_ARR[@]}"; do
         RUN_MARIADB=1
     elif [[ "${element}"x == "db2"x ]];then
         RUN_DB2=1
+    elif [[ "${element}"x == "lakesoul"x ]]; then
+        RUN_LAKESOUL=1
     else
         echo "Invalid component: ${element}"
         usage
@@ -462,4 +467,36 @@ if [[ "${RUN_MARIADB}" -eq 1 ]]; then
         sudo rm "${ROOT}"/docker-compose/mariadb/data/* -rf
         sudo docker compose -f "${ROOT}"/docker-compose/mariadb/mariadb-10.yaml --env-file "${ROOT}"/docker-compose/mariadb/mariadb-10.env up -d
     fi
+fi
+
+if [[ "${RUN_LAKESOUL}" -eq 1 ]]; then
+    echo "RUN_LAKESOUL"
+    cp "${ROOT}"/docker-compose/lakesoul/lakesoul.yaml.tpl "${ROOT}"/docker-compose/lakesoul/lakesoul.yaml
+    sed -i "s/doris--/${CONTAINER_UID}/g" "${ROOT}"/docker-compose/lakesoul/lakesoul.yaml
+    sudo docker compose -f "${ROOT}"/docker-compose/lakesoul/lakesoul.yaml down
+    sudo rm -rf "${ROOT}"/docker-compose/lakesoul/data
+    if [[ "${STOP}" -ne 1 ]]; then
+        echo "PREPARE_LAKESOUL_DATA"
+        sudo docker compose -f "${ROOT}"/docker-compose/lakesoul/lakesoul.yaml up -d
+    fi
+    ## import tpch data into lakesoul
+    ## install rustup
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --default-toolchain none -y
+    ## install rust nightly-2023-05-20
+    rustup install nightly-2023-05-20
+    ## download&generate tpch data
+    mkdir -p lakesoul/test_files/tpch/data
+    git clone https://github.com/databricks/tpch-dbgen.git
+    cd tpch-dbgen
+    make
+    ./dbgen -f -s 0.1
+    mv *.tbl ../lakesoul/test_files/tpch/data
+    cd ..
+    export TPCH_DATA=`realpath lakesoul/test_files/tpch/data`
+    ## import tpch data
+    git clone https://github.com/lakesoul-io/LakeSoul.git
+#    git checkout doris_dev
+    cd LakeSoul/rust
+    cargo test load_tpch_data --package lakesoul-datafusion --features=ci -- --nocapture
+
 fi
